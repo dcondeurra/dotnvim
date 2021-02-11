a7ab9050a67cc10395fc43c27773ab3745a3a2dd
Daniel <dconde.urra@gmail.com>
2021-02-11 00:49:09 -0300

    Include history.

--- lua/nvim/plugins/fzf/init.lua
+++ lua/nvim/plugins/fzf/init.lua
@@ -1,3 +1,4 @@
 Keybind.g({
 	-- { 'n', '<c-p>', [[<cmd>call fzf#run(fzf#wrap({'source': 'git ls-files --exclude-standard --others --cached'}))<cr>]], {} },
+	{ 'n', '<leader>h', '<cmd>History<cr>', silent_opt },
 })
